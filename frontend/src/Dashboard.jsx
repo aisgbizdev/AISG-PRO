4048137106f57e600cbe444fe5b12936e1929749
aisgbizdev <aisgbizdev@gmail.com>
2025-10-29 23:49:28 +0700

    Update Dashboard.jsx

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Login from "./Login";
 import Dashboard from "./Dashboard"; // rename App ke Dashboard.jsx
+import CoachingPanel from "./CoachingPanel";
 
 export default function App() {
   const [role, setRole] = useState(null);
