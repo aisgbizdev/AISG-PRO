44b8862c7defdf4c16b314305b1f03e0a5ae6205
aisgbizdev <aisgbizdev@gmail.com>
2025-10-31 22:56:18 +0700

    Delete frontend directory

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import Login from "./Login";
-import Dashboard from "./Dashboard";
-
-export default function App() {
-  const [role, setRole] = useState(null);
-  if (!role) return <Login onLogin={setRole} />;
-  return <Dashboard role={role} />;
-}
